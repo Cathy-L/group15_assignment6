8fd6262af418f901d30574ea9fdb92f8ac5cf860
Cathy Li <cathysli@outlook.com>
2018-10-18 09:40:03 -0500

    Initial Commit

---
+++ group15_assignment6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  group15_assignment6
+//
+//  Created by Cathy Li on 10/18/18.
+//  Copyright © 2018 CS329E. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
