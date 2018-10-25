668fb2f66072f1b960b7be5a0e5b1531e9150119
Nabeel Masood <nabeelmasood@wireless-10-145-142-118.public.utexas.edu>
2018-10-25 13:56:30 -0500

    Quest ViewController

---
+++ group15_assignment6/QuestViewController.swift
@@ -0,0 +1,40 @@
+//
+//  QuestViewController.swift
+//  group15_assignment6
+//
+//  Created by Nabeel Masood on 10/25/18.
+//  Copyright © 2018 CS329E. All rights reserved.
+//
+
+import UIKit
+
+class QuestViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var lvlLabel: UILabel!
+    @IBOutlet weak var professionLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var hpLabel: UILabel!
+    @IBOutlet weak var pictureView: UIImageView!
+    
+    @IBOutlet weak var questView: UITextView!
+    
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
